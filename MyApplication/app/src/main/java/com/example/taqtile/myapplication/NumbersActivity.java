a3295797f6dc9ce01ce06cfa64f71fbe0a6ac8e2
Taqtile <taqtile@GreenCow.local>
2017-09-11 11:27:27 -0300

    miwokk app

---
+++ MyApplication/app/src/main/java/com/example/taqtile/myapplication/NumbersActivity.java
@@ -0,0 +1,19 @@
+package com.example.taqtile.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class NumbersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_numbers);
+
+    }
+}
